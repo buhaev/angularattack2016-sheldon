096f767774e05d0e7bc26fb1ccb861f49cf50517
Ivan Bugaev <ivan.bugaev@solomoto.com>
2016-05-14 16:19:00 +0300

    Add angular/material2

--- client/app/core/components/app/app.component.js
+++ client/app/core/components/app/app.component.js
@@ -6,9 +6,24 @@ import { MenuComponent } from '../menu/menu.component';
 import { LoggedInRouterOutletDirective } from '../../../auth';
 import { routes } from './router.config';
 
+import {MdButton} from '@angular2-material/button';
+import {MdCard} from '@angular2-material/card';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdIcon} from '@angular2-material/icon';
+import {MdInput} from '@angular2-material/input';
+import {MdList} from '@angular2-material/list';
+import {MdProgressBar} from '@angular2-material/progress-bar';
+import {MdProgressCircle} from '@angular2-material/progress-circle';
+import {MdRadioButton} from '@angular2-material/radio';
+import {MdSidenav} from '@angular2-material/sidenav';
+import {MdToolbar} from '@angular2-material/toolbar';
+
 @Component({
   selector: 'my-app',
-  directives: [LoggedInRouterOutletDirective, MenuComponent],
+  directives: [
+    LoggedInRouterOutletDirective, MenuComponent, MdButton, MdCard, MdCheckbox, MdIcon, MdInput, MdList,
+    MdProgressBar, MdProgressCircle, MdRadioButton, MdSidenav, MdToolbar 
+  ],
   template: template
 })
 @RouteConfig(routes)
