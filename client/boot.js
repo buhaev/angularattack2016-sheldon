023a337949406e94bf5a9a55be46dd7c71898143
Anton Kuznetsov <isnifer@gmail.com>
2016-05-14 12:01:02 +0300

    Update to ES6 Starter

---
+++ client/boot.js
@@ -0,0 +1,24 @@
+import './shim';
+import 'rxjs/add/operator/map';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { enableProdMode, provide } from '@angular/core';
+import { AppComponent } from './app/core/components/app/app.component';
+import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { FORM_PROVIDERS, LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { AUTH_PROVIDERS } from './app/auth';
+import { POSTS_PROVIDERS } from './app/posts';
+
+if (ENVIRONMENT === 'production') {
+  enableProdMode();
+}
+
+bootstrap(AppComponent, [
+  FORM_PROVIDERS,
+  HTTP_PROVIDERS,
+  ROUTER_PROVIDERS,
+  AUTH_PROVIDERS,
+  POSTS_PROVIDERS,
+  provide(LocationStrategy, { useClass: HashLocationStrategy }),
+  provide('ENVIRONMENT', { useValue: ENVIRONMENT })
+]);
