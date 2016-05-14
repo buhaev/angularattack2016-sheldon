99d35c6b0a3bd21da39b59eb20285bfb1ffbe55f
Anton Kuznetsov <isnifer@gmail.com>
2016-05-14 13:00:38 +0300

    Update to ES6 Starter (#1)
    * Update to ES6 Starter
    
    * Update package.json

---
+++ client/setup.spec.js
@@ -0,0 +1,31 @@
+import 'phantomjs-polyfill';
+import './shim';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/async-test';
+
+import {
+  TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
+} from '@angular/platform-browser-dynamic/testing/browser';
+import {
+  setBaseTestProviders,
+  afterEach, beforeEach, beforeEachProviders,
+  describe, it, expect,
+  inject, injectAsync,
+  async, tick
+} from '@angular/core/testing';
+
+setBaseTestProviders(TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+
+Object.assign(global, {
+  afterEach, beforeEach, beforeEachProviders,
+  describe, it, expect,
+  inject, injectAsync,
+  async, tick
+});
+
+Error.stackTraceLimit = Infinity;
+
+let testContext = require.context('./app', true, /\.spec\.js/);
+testContext.keys().forEach(testContext);
