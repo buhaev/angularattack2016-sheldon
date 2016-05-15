fcd30a3276724e80f2952eb754e7954d78bbdbc3
StGeass <st.geass@gmail.com>
2016-05-15 22:44:36 +0300

    [f/newEventForm] After merge [2]

--- src/app/core/components/app/router.config.js
+++ src/app/core/components/app/router.config.js
@@ -4,7 +4,7 @@ import {PostEditComponent} from '../../../posts/components/post-edit/post-edit.c
 import {AboutComponent} from '../about/about.component';
 import {LoginComponent} from '../../../auth/components/login/login.component';
 
-import {CalendarComponent} from '../../../calendar/components/calendar.component';
+import {CalendarComponent} from '../../../calendar/components/calendar/calendar.component';
 
 export const routes = [
     {path: '/', component: CalendarComponent, name: 'Calendar', useAsDefault: true}
