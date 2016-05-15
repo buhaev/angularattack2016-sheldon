1ba956897742d7b446eae10f0744f47b90b1f261
StGeass <st.geass@gmail.com>
2016-05-15 11:30:08 +0300

    [f/calendarService] Initial service functional

--- src/app/posts/components/post-list/post-list.component.js
+++ src/app/posts/components/post-list/post-list.component.js
@@ -3,24 +3,33 @@ import { ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
 import template from './post-list.template.html';
 import { PostService } from '../../services/post/post.service';
+import { CalendarService } from '../../../calendar/services/calendar.service';
 import { PostListItemComponent } from '../post-list-item/post-list-item.component';
 
 @Component({
-  selector: 'post-list',
-  template: template,
-  directives: [ROUTER_DIRECTIVES, PostListItemComponent],
-  changeDetection: ChangeDetectionStrategy.Detached
+    selector: 'post-list',
+    template: template,
+    directives: [ROUTER_DIRECTIVES, PostListItemComponent],
+    changeDetection: ChangeDetectionStrategy.Detached
 })
 export class PostListComponent {
-  constructor(postService: PostService) {
-    this._postService = postService;
-  }
+    constructor (postService:PostService, CalendarService:CalendarService) {
+        this._postService = postService;
+        this._calendarService = CalendarService;
+    }
 
-  ngOnInit() {
-    this._postService.refreshPosts();
-  }
+    ngOnInit () {
+        //this._postService.refreshPosts();
+        this._calendarService.listUpcomingEvents();
+        //CalendarService
+    }
 
-  getRemotePosts() {
-    return this._postService.remotePosts;
-  }
+    getRemotePosts () {
+        return this._postService.remotePosts;
+    }
+
+    getRemoteEvents () {
+        console.log(this._calendarService.remoteEvents.value);
+        return this._calendarService.remoteEvents;
+    }
 }
